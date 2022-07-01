2e90d23cf57948bbe052fd8eaca1e5fad2088bb4
Berend Wouters <berend.wouters@gmail.com>
2022-07-01 21:30:52 +0200

    feat: some new UI aspects introduced

--- WebApp/doorrequest/src/app/modules/dashboard/dashboard.module.ts
+++ WebApp/doorrequest/src/app/modules/dashboard/dashboard.module.ts
@@ -1,25 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AuthService } from '../core/services/auth.service';
-import { DoorService } from '../core/services/door.service';
-import { SharedModule } from '../shared/shared.module';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { AuthService } from "../core/services/auth.service";
+import { DoorService } from "../core/services/door.service";
+import { SharedModule } from "../shared/shared.module";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
 
-const routes: Routes = [{path: '', component: DashboardComponent}]
+const routes: Routes = [{ path: "", component: DashboardComponent }];
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     MatSnackBarModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
     RouterModule.forChild(routes),
     CommonModule,
     SharedModule,
   ],
-  providers:[
-    AuthService,
-    DoorService
-  ]
+  providers: [AuthService, DoorService],
 })
-export class DashboardModule { }
+export class DashboardModule {}
--- WebApp/doorrequest/src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ WebApp/doorrequest/src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -1,50 +1,66 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { UntypedFormGroup, UntypedFormControl } from '@angular/forms';
-import { DoorService } from '../../core/services/door.service';
-import { AuthService } from '../../core/services/auth.service';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { UntypedFormGroup, UntypedFormControl } from "@angular/forms";
+import { DoorService } from "../../core/services/door.service";
+import { AuthService } from "../../core/services/auth.service";
+import { UserService } from "../../core/services/user.service";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"],
 })
 export class DashboardComponent implements OnInit {
   setupImage: string;
   form: UntypedFormGroup;
+
+  openDoor = true;
   constructor(
     private doorService: DoorService,
     private authService: AuthService,
+    private userService: UserService,
     public dialog: MatDialog,
     private snackBar: MatSnackBar
   ) {}
 
   ngOnInit() {
     this.form = new UntypedFormGroup({
-      validationKey: new UntypedFormControl('XXXXXX')
+      validationKey: new UntypedFormControl("XXXXXX"),
     });
 
-    this.authService.setupQRCode().subscribe(res => {
+    this.authService.setupQRCode().subscribe((res) => {
       this.setupImage = res.image;
     });
   }
   submit() {
     if (this.form.valid) {
-      const validationKey = this.form.get('validationKey');
+      const validationKey = this.form.get("validationKey");
       const validationNumber = Number(validationKey.value);
       if (!isNaN(validationNumber)) {
         this.doorService.openDoorRequest(validationNumber).subscribe(
-          res =>
-            this.snackBar.open('Request to open the door has been sent', 'OK', {
+          (res) =>
+            this.snackBar.open("Request to open the door has been sent", "OK", {
               duration: 2000,
-              verticalPosition: 'top'
+              verticalPosition: "top",
             }),
-          error => {
-            this.snackBar.open('Failed to request to open the door', 'OK');
+          (error) => {
+            this.snackBar.open("Failed to request to open the door", "OK");
           }
         );
       }
     }
+  }
+
+  showOpenDoor() {
+    this.openDoor = true;
+  }
+
+  showSetup() {
+    this.openDoor = false;
+  }
+
+  logout() {
+    this.userService.logout();
   }
 }
