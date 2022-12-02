9c745218b8719b9e5bc9572194714ef4c6da6c4f
Berend Wouters <berend.wouters@gmail.com>
2022-12-02 19:02:46 +0100

    fix: logging in on iOS doesn't work

--- WebApp/doorrequest/src/app/modules/login/login-form/login-form.component.ts
+++ WebApp/doorrequest/src/app/modules/login/login-form/login-form.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from "@angular/core";
-import { UntypedFormGroup, UntypedFormControl } from "@angular/forms";
+import {
+  UntypedFormGroup,
+  UntypedFormControl,
+  Validators,
+} from "@angular/forms";
 import { Router, ActivatedRoute } from "@angular/router";
 import { UserService } from "../../core/services/user.service";
 
 @Component({
   selector: "app-login-form",
   templateUrl: "./login-form.component.html",
-  styleUrls: ["./login-form.component.scss"]
+  styleUrls: ["./login-form.component.scss"],
 })
 export class LoginFormComponent implements OnInit {
   form: UntypedFormGroup;
@@ -18,29 +22,32 @@ export class LoginFormComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.activated.queryParams.subscribe(params => {
+    this.activated.queryParams.subscribe((params) => {
       this.returnUrl = params["returnUrl"];
     });
 
     this.form = new UntypedFormGroup({
-      username: new UntypedFormControl(null),
-      password: new UntypedFormControl(null)
+      username: new UntypedFormControl(null, [Validators.required]),
+      password: new UntypedFormControl(null, [Validators.required]),
     });
   }
 
   submit() {
     const username = this.form.get("username").value;
     const password = this.form.get("password").value;
-    this.userService.authenticate(username, password).subscribe(
-      () => {
-        const url = this.returnUrl || "/";
-        this.router.navigate([url]);
-      },
-      error => {
-        this.form.setErrors({
-          notAuthorized: true
-        });
-      }
-    );
+    console.log(this.form);
+    if (this.form.valid) {
+      this.userService.authenticate(username, password).subscribe(
+        () => {
+          const url = this.returnUrl || "/";
+          this.router.navigate([url]);
+        },
+        (error) => {
+          this.form.setErrors({
+            notAuthorized: true,
+          });
+        }
+      );
+    }
   }
 }
